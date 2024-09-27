9607f1851684c91b02991f47150c897ede3589c6
MounikaRangisetty <83297162+MounikaRangisetty@users.noreply.github.com>
2024-09-27 14:22:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask
-from prometheus_flask_exporter import PrometheusMetrics
+import logging
 
 app = Flask(__name__)
-metrics = PrometheusMetrics(app)
+
+# Set up logging to a file
+logging.basicConfig(filename='/var/log/myapp/flask.log', level=logging.INFO)
 
 @app.route('/')
 def hello():
+    app.logger.info("Accessed '/' route")
     return "Hello, World!"
 
 if __name__ == '__main__':
