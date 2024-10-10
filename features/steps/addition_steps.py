75f199731fb87ed405c31e9ce8da99dfe396fdef
MounikaRangisetty <83297162+MounikaRangisetty@users.noreply.github.com>
2024-10-10 14:26:23 +0530

    Create addition_steps.py

---
+++ features/steps/addition_steps.py
@@ -0,0 +1,24 @@
+from behave import given, when, then
+import requests
+
+BASE_URL = 'http://43.203.121.184:5000'
+
+@given('the application is running')
+def step_impl(context):
+    pass  # Assumes the app is already running
+
+@when('I send a request to add {x} and {y}')
+def step_impl(context, x, y):
+    if y == 'no':
+        response = requests.get(f'{BASE_URL}/add?x={x}')
+    else:
+        response = requests.get(f'{BASE_URL}/add?x={x}&y={y}')
+    context.response = response
+
+@then('I receive a response with the result {result}')
+def step_impl(context, result):
+    assert context.response.json()['result'] == int(result)
+
+@then('I receive a {status_code} error')
+def step_impl(context, status_code):
+    assert context.response.status_code == int(status_code)
